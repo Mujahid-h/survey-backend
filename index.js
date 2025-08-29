01a2ea60a98994a3e6ea89ea66f0ca5fdaa26396
Mujahid corporate prism <c.prism2002@gmail.com>
2025-08-29 11:47:25 +0500

    check

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import verifyCheckout from "./routes/verify-checkout.js";
+// import orderConfirmation from "./routes/order-confirmation.js";
+// import payment from "./routes/payment.js";
+// import connectDB from "./config/db.js";
+// import surveyRoutes from "./routes/surveyRoutes.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+
+// //  Test route
+// app.get("/", (req, res) => {
+//   res.send("Server is running here successfully!");
+// });
+
+// // Routes
+// app.use("/api/checkout", verifyCheckout);
+// app.use("/api/order", orderConfirmation);
+// app.use("/api/payment", payment);
+// app.use("/api/surveys", surveyRoutes);
+// const PORT = 8080;
+
+// app.listen(PORT, () => {
+//   connectDB();
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -9,8 +46,10 @@ import surveyRoutes from "./routes/surveyRoutes.js";
 
 dotenv.config();
 
+// Initialize app
 const app = express();
 
+// Middlewares
 app.use(express.json());
 app.use(
   cors({
@@ -18,7 +57,10 @@ app.use(
   })
 );
 
-//  Test route
+// Connect to DB
+connectDB();
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Server is running here successfully!");
 });
@@ -28,9 +70,6 @@ app.use("/api/checkout", verifyCheckout);
 app.use("/api/order", orderConfirmation);
 app.use("/api/payment", payment);
 app.use("/api/surveys", surveyRoutes);
-const PORT = 8080;
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Export the app for Vercel
+export default app;
